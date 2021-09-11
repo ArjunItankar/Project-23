74fe18f8b7299fbcf21f830607f9604e4e1b5f1c
ArjunItankar <86589224+ArjunItankar@users.noreply.github.com>
2021-09-11 12:18:46 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase, playerArcher;
+var archer;
+var baseimage, playerimage, archerimage;
+
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+  archerimage = loadImage("./assets/playerArcher.png");
+
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  //create player base body
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  //create player body
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world, player);
+
+  //player archer body
+  playerArcher = Bodies.rectangle(player.position.x + 30, 210, 200, 200, options);
+  World.add(world, playerArcher);
+}
+
+function draw() {
+  background(backgroundImg);
+
+  //show the player image using image() function
+  //show the playerbase image using image() function
+  Engine.update(engine);
+  image(baseimage, playerBase.position.x, playerBase.position.y, 180, 150);
+  image(playerimage, player.position.x, player.position.y, 70, 180);
+  image(archerimage, playerArcher.position.x, playerArcher.position.y, 90, 100);
+  
+  playerArcher.display();
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+}
